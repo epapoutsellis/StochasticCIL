226df75558f465a1c197805724f899e5f7478d05
epapoutsellis <epapoutsellis@gmail.com>
2023-07-07 06:26:38 +0100

    add herman sampling in SVRG

--- Wrappers/Python/cil/optimisation/utilities/__init__.py
+++ Wrappers/Python/cil/optimisation/utilities/__init__.py
@@ -2,9 +2,9 @@ from .RandomSampling import RandomSampling
 from .RandomSampling import RandomIndex
 from .RandomSampling import RandomBatch
 from .SequentialSampling import SequentialSampling
-from .HermanMeyerSampling import HermanMeyerSampling, herman_meyer_order
-from .DistanceError import OptimalityDistance
-from .DistanceError import ConsecutiveIterationsDistance
+# from .HermanMeyerSampling import HermanMeyerSampling, herman_meyer_order
+from .DistanceError import OptimalityDistance, DistanceError, ConsecutiveIterationsDistance
+
 
 
 
